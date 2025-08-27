e992cf4ecfe08a5b37658044decca0b1305821bf
Swakanyi <serahwakanyi@gmail.com>
2025-08-27 10:11:09 +0300

    Week-one-project-react

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Search from './Search'
 import MovieList from './MovieList'
+import Movies from './Movies';
 
 const sampleMovies = [
   {
@@ -56,6 +57,7 @@ function App() {
       <h1>BingeBox</h1>
       <Search onSearch={fetchMovies} />
       <MovieList movies={movies} />
+      <Movies />
     </div>
   );
 }
